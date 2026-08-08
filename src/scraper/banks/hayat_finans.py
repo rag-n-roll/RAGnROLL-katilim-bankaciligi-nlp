067df3c2f7c7ad330bed58c6473e40ed7efc2352
Kutay32 <kutay.oralli@hotmail.com>
2026-08-08 23:05:08 +0300

    feat: cover all BDDK participation banks

---
+++ src/scraper/banks/hayat_finans.py
@@ -0,0 +1,14 @@
+from ..base import BaseBankScraper, ScraperConfig
+
+
+class HayatFinansScraper(BaseBankScraper):
+    config = ScraperConfig(
+        slug="hayat-finans",
+        bank_name="Hayat Finans Katılım Bankası A.Ş.",
+        base_url="https://hayatfinans.com.tr",
+        listing_urls=("https://hayatfinans.com.tr/kampanyalar",),
+        detail_pattern=r"/kampanyalar/[^/?#]+$",
+        listing_link_selectors=("section#contentCardContainer a[href]",),
+        content_selectors=("div[id^='contentModuleSection']",),
+        title_selectors=("main h1", "h1"),
+    )
